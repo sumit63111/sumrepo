6144348186e4e14ad6d030360cd017741ed675f1
sumit63111 <sumit63111@gmail.com>
2024-07-31 11:52:03 +0530

    this is scond commit

--- first.py
+++ first.py
@@ -1,2 +1,13 @@
-age = int(input("Enter the age "))
-print(age)
+# age = int(input("Enter the age "))
+# print(age)
+
+# a=int(input("enter the first number "))
+# b=int(input("enter the first number "))
+# c= a + b
+# print(c)
+
+exp1=eval(input())
+print(exp1)
+
+print("hello world")
+
